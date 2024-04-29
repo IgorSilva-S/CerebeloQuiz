0acf6d0aa383afe29844efd9a6a7199cc26dbae7
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2024-04-29 10:08:53 -0300

    Close Window

--- src/index.js
+++ src/index.js
@@ -91,6 +91,9 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
+ipcMain.on("closeQuiz", () => {
+  app.quit()
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
